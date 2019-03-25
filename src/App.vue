f4bfa528108213802f576ed3745b5b5983bdf841
danieltalhaug <daniel.talhaug@outlook.com>
2019-03-25 13:52:44 +0100

    Set up VUE.js and updated readme

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <LoginPage/>
+  </div>
+</template>
+
+<script>
+import LoginPage from './components/LoginPage.vue'
+
+export default {
+  name: 'app',
+  components: {
+    LoginPage
+  }
+}
+</script>
+
+<style lang="scss">
+
+#app {
+ 
+}
+</style>
---
+++ src/components/LoginPage.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>LoginPage works</h1>
+</template>
+
+export default {
+  name: 'LoginPage',
+  props {
+    msg: string;
+
+  }
+}
+
+<style lang="scss">
+  body {
+    background-color: #eee;
+  }
+</style>
