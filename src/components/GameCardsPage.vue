e1b688e120461f7ae34e28088ff4dcf6bc94ab93
danieltalhaug <daniel.talhaug@outlook.com>
2019-03-28 12:26:53 +0100

    created pages and component for game cards

---
+++ src/components/GameCardsPage.vue
@@ -0,0 +1,41 @@
+<template>
+    
+</template>
+
+<script>
+import GameCardsComponent from 'GameCardsComponent.vue'
+
+export default {
+    name: 'GameCardsPage',
+    components: {
+        GameCardsComponent
+    },
+    data() {
+        return {
+            cards: []
+        }
+    },
+    beforeMount: function() {
+        const app = this;
+        const corsUrl = 'https://cors-anywhere.herokuapp.com/';
+        const gameUrl = 'https://api.magicthegathering.io/v1/cards';
+
+        fetch(corsUrl + gameUrl)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(result) {
+            app.cards = result;
+        })
+    }
+}
+</script>
+<style lang="scss">
+// Variables
+$grey: #ced4da;
+$border-radius: 5px;
+$error-red: #ff5b5b;
+// SCSS
+
+
+</style>
